f921d7c5b18f37f71a074865491ee60861e935f5
Akihiro ITO <akihiro@harapeco.club>
2026-05-11 00:34:12 +0900

    環境変数にcronのシークレットを定義する

--- api/process.d.ts
+++ api/process.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly CHANNEL_ACCESS_TOKEN: string;
     readonly CHANNEL_SECRET: string;
+    readonly CRON_SECRET: string;
     readonly PORT: string | undefined;
     readonly ENVIRONMENT: string;
     readonly DOMAIN_NAME: string;
