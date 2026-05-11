f294cd73a63fc2d4d5b755c02a78b01c30ce0281
Akihiro ITO <akihiro@harapeco.club>
2026-05-11 14:32:25 +0900

    refs #3 イベントコードのオーバーフローを修正する

--- app/definitions/definitions.ts
+++ app/definitions/definitions.ts
@@ -1,5 +1,5 @@
 import type { Concert } from '../types';
-import FUJITA from './20260712_fujita_niigata';
+import FUJITA from './20260712_fujita';
 import OPERA_CITY from './20260910_opera_city';
 
 const DEFINITIONS: Concert[] = [FUJITA, OPERA_CITY];
