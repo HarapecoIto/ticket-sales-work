86b083fc9474054e71aa56f93c1ede464a10b09a
Akihiro ITO <akihiro@harapeco.club>
2026-05-19 16:08:49 +0900

    refs #15 不要なコードを削除する

--- app/line/webhook/route.ts
+++ app/line/webhook/route.ts
@@ -68,13 +68,6 @@ const handleEvent = async (
   }
   console.log(`[line] got sourceId: ${sourceId}`);
 
-  // 1時間以上前の状態は削除してクリーンアップする
-  await prisma.conversation_state.deleteMany({
-    where: {
-      updated_at: { lt: new Date(Date.now() - 60 * 60 * 1000) },
-    },
-  });
-
   // 販売状況の通知
   const notificationMessages: messagingApi.Message[] | null = await notificator(sourceId, event);
   if (notificationMessages) {
