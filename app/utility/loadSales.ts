3b39d515ac6679ccc3e0b1ffab1d3e44014d624a
Akihiro ITO <akihiro@harapeco.club>
2026-05-25 16:56:59 +0900

    refs #37 使用していないimportを削除する

--- app/utility/loadSales.ts
+++ app/utility/loadSales.ts
@@ -1,12 +1,5 @@
 import prisma from '@/lib/prisma';
-import {
-  type Tour,
-  type Concert,
-  type Campaign,
-  type Distribution,
-  type TicketSales,
-} from '@/app/types';
-import TOURS from '@/app/definitions/definitions';
+import { type Tour, type Concert, type Campaign, type TicketSales } from '@/app/types';
 
 export const getTicketSales = async (tour: Tour, c: Concert): Promise<TicketSales[]> => {
   // 24時間以内に集計されたレコードを取得する
