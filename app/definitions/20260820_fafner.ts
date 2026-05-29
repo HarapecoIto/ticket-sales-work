ef1102a5d70ff50ca5583a6266fbb6e8839445fb
Akihiro ITO <akihiro@harapeco.club>
2026-05-30 00:03:56 +0900

    refs #45 ファフナー の定義ファイルを作成する

---
+++ app/definitions/20260820_fafner.ts
@@ -0,0 +1,153 @@
+import type { Tour, Concert, Campaign } from '../types';
+
+const CONCERTS: Concert[] = [
+  {
+    name: '蒼穹のファフナー Symphony Orchestra Concert 2026',
+    short_name: 'Day 1',
+    date_at: new Date('2026-08-20T19:00:00+09:00'),
+    hall: 'すみだトリフォニーホール　大ホール',
+    tickets: [
+      {
+        name: 'ＳＳ席',
+      },
+      {
+        name: 'Ｓ席',
+      },
+      {
+        name: 'Ａ席',
+      },
+      {
+        name: '車椅子席',
+      },
+    ],
+    distribution: [
+      {
+        campaign: 'ファフナー公式サイト最速先行',
+        play_guide: 'eplus',
+        ticket: 'ＳＳ席',
+        campaign_alias: '［ＤＡＹ１］ファフナー公式サイト最速先行',
+        ticket_alias: 'ＳＳ席',
+      },
+      {
+        campaign: 'ｇｅｌａｍｉｌｙ最速先行',
+        play_guide: 'eplus',
+        ticket: 'Ｓ席',
+        campaign_alias: '［ＤＡＹ１］ｇｅｌａｍｉｌｙ最速先行',
+        ticket_alias: 'Ｓ席',
+      },
+      {
+        campaign: '２次抽選先行',
+        play_guide: 'eplus',
+        ticket: 'Ａ席',
+        campaign_alias: '［ＤＡＹ１］２次抽選先行',
+        ticket_alias: 'Ａ席',
+      },
+      {
+        campaign: '一般発売',
+        play_guide: 'eplus',
+        ticket: '車椅子席',
+        campaign_alias: '★［ＤＡＹ１］一般発売',
+        ticket_alias: '車椅子席',
+      },
+    ],
+  },
+  {
+    name: '蒼穹のファフナー Symphony Orchestra Concert 2026',
+    short_name: 'Day 2',
+    date_at: new Date('2026-08-21T19:00:00+09:00'),
+    hall: 'すみだトリフォニーホール　大ホール',
+    tickets: [
+      {
+        name: 'ＳＳ席',
+      },
+      {
+        name: 'Ｓ席',
+      },
+      {
+        name: 'Ａ席',
+      },
+      {
+        name: '車椅子席',
+      },
+    ],
+    distribution: [
+      {
+        campaign: 'ファフナー公式サイト最速先行',
+        play_guide: 'eplus',
+        ticket: 'ＳＳ席',
+        campaign_alias: '［ＤＡＹ２］ファフナー公式サイト最速先行',
+        ticket_alias: 'ＳＳ席',
+      },
+      {
+        campaign: 'ｇｅｌａｍｉｌｙ最速先行',
+        play_guide: 'eplus',
+        ticket: 'Ｓ席',
+        campaign_alias: '［ＤＡＹ２］ｇｅｌａｍｉｌｙ最速先行',
+        ticket_alias: 'Ｓ席',
+      },
+      {
+        campaign: '２次抽選先行',
+        play_guide: 'eplus',
+        ticket: 'Ａ席',
+        campaign_alias: '［ＤＡＹ２］２次抽選先行',
+        ticket_alias: 'Ａ席',
+      },
+      {
+        campaign: '一般発売',
+        play_guide: 'eplus',
+        ticket: '車椅子席',
+        campaign_alias: '★［ＤＡＹ２］一般発売',
+        ticket_alias: '車椅子席',
+      },
+    ],
+  },
+];
+
+const CAMPAIGNS: Campaign[] = [
+  {
+    campaign_name: 'ファフナー公式サイト最速先行',
+    campaign_type: 'ByLottery', // 抽選販売
+    reservation_start_at: null,
+    reservation_end_at: null,
+    lottery_at: null,
+    settlement_at: null,
+    sales_start_at: null,
+  },
+  {
+    campaign_name: 'ｇｅｌａｍｉｌｙ最速先行',
+    campaign_type: 'FirstCome',
+    reservation_start_at: null,
+    reservation_end_at: null,
+    lottery_at: null,
+    settlement_at: null,
+    sales_start_at: new Date('2026-04-01T10:00:00+09:00'), // 発売日
+  },
+  {
+    campaign_name: '２次抽選先行',
+    campaign_type: 'FirstCome',
+    reservation_start_at: null,
+    reservation_end_at: null,
+    lottery_at: null,
+    settlement_at: null,
+    sales_start_at: new Date('2026-04-01T10:00:00+09:00'), // 発売日
+  },
+  {
+    campaign_name: '一般発売',
+    campaign_type: 'FirstCome',
+    reservation_start_at: null,
+    reservation_end_at: null,
+    lottery_at: null,
+    settlement_at: null,
+    sales_start_at: new Date('2026-04-01T10:00:00+09:00'), // 発売日
+  },
+];
+
+const DEFINITIONS: Tour = {
+  event_code: '20260820_fafner',
+  name: '蒼穹のファフナー Symphony Orchestra Concert 2026',
+  short_name: '蒼穹のファフナー',
+  campaigns: CAMPAIGNS,
+  concerts: CONCERTS,
+};
+
+export default DEFINITIONS;
--- app/definitions/definitions.ts
+++ app/definitions/definitions.ts
@@ -5,7 +5,8 @@ import KOBE_BOUKEN from './20260705_chino_kobe';
 import TOKYO_BOUKEN from './20260724_chino_tokyo';
 import SHIN_SUMMER from './20260719_shin_summer';
 import TABICLA from './20260825_tabicla';
+import FAFNER from './20260820_fafner';
 
-const TOURS: Tour[] = [FUJITA, OPERA_CITY, KOBE_BOUKEN, TOKYO_BOUKEN, SHIN_SUMMER, TABICLA];
+const TOURS: Tour[] = [FUJITA, OPERA_CITY, KOBE_BOUKEN, TOKYO_BOUKEN, SHIN_SUMMER, TABICLA, FAFNER];
 
 export default TOURS;
