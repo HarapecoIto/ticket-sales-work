e9b3264f8383e78bb2ffd33473f954b9f1bc5e8a
Akihiro ITO <akihiro@harapeco.club>
2026-05-29 23:42:39 +0900

    refs #45 旅するクラシックの定義ファイルを作成する

---
+++ app/definitions/20260825_tabicla.ts
@@ -0,0 +1,66 @@
+import type { Tour, Concert, Campaign } from '../types';
+
+const CONCERTS: Concert[] = [
+  {
+    name: '旅するクラシック プレミアム・ステージ 2026',
+    short_name: '旅するクラシック',
+    date_at: new Date('2026-08-25T18:19:00+09:00'),
+    hall: 'すみだトリフォニーホール 大ホール',
+    tickets: [
+      {
+        name: 'ファーストクラス（S席）',
+      },
+      {
+        name: 'ビジネスクラス（A席）',
+      },
+      {
+        name: 'エコノミークラス（B席）',
+      },
+    ],
+    distribution: [
+      {
+        campaign: '一般販売',
+        play_guide: 'teket',
+        ticket: 'ファーストクラス（S席）',
+        campaign_alias: '一般販売',
+        ticket_alias: 'ファーストクラス（S席）',
+      },
+      {
+        campaign: '一般販売',
+        play_guide: 'teket',
+        ticket: 'ビジネスクラス（A席）',
+        campaign_alias: '一般販売',
+        ticket_alias: 'ビジネスクラス（A席）',
+      },
+      {
+        campaign: '一般販売',
+        play_guide: 'teket',
+        ticket: 'エコノミークラス（B席）',
+        campaign_alias: '一般販売',
+        ticket_alias: 'エコノミークラス（B席）',
+      },
+    ],
+  },
+];
+
+const CAMPAIGNS: Campaign[] = [
+  {
+    campaign_name: '一般販売',
+    campaign_type: 'FirstCome', // 先着販売
+    reservation_start_at: null,
+    reservation_end_at: null,
+    lottery_at: null,
+    settlement_at: null,
+    sales_start_at: null,
+  },
+];
+
+const DEFINITIONS: Tour = {
+  event_code: '20260825_tabicla',
+  name: '旅するクラシック プレミアム・ステージ 2026',
+  short_name: '旅するクラシック',
+  campaigns: CAMPAIGNS,
+  concerts: CONCERTS,
+};
+
+export default DEFINITIONS;
--- app/definitions/definitions.ts
+++ app/definitions/definitions.ts
@@ -4,7 +4,8 @@ import OPERA_CITY from './20260910_opera_city';
 import KOBE_BOUKEN from './20260705_chino_kobe';
 import TOKYO_BOUKEN from './20260724_chino_tokyo';
 import SHIN_SUMMER from './20260719_shin_summer';
+import TABICLA from './20260825_tabicla';
 
-const TOURS: Tour[] = [FUJITA, OPERA_CITY, KOBE_BOUKEN, TOKYO_BOUKEN, SHIN_SUMMER];
+const TOURS: Tour[] = [FUJITA, OPERA_CITY, KOBE_BOUKEN, TOKYO_BOUKEN, SHIN_SUMMER, TABICLA];
 
 export default TOURS;
