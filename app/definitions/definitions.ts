820cc3f763e4f6db09189a1b4bcc1b19a7e70d2e
Akihiro ITO <akihiro@harapeco.club>
2026-05-29 23:36:18 +0900

    refs #45 シン・サマー・オーケストラの定義ファイルを作成する

--- app/definitions/definitions.ts
+++ app/definitions/definitions.ts
@@ -3,7 +3,8 @@ import FUJITA from './20260712_fujita';
 import OPERA_CITY from './20260910_opera_city';
 import KOBE_BOUKEN from './20260705_chino_kobe';
 import TOKYO_BOUKEN from './20260724_chino_tokyo';
+import SHIN_SUMMER from './20260719_shin_summer';
 
-const TOURS: Tour[] = [FUJITA, OPERA_CITY, KOBE_BOUKEN, TOKYO_BOUKEN];
+const TOURS: Tour[] = [FUJITA, OPERA_CITY, KOBE_BOUKEN, TOKYO_BOUKEN, SHIN_SUMMER];
 
 export default TOURS;
