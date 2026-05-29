9388381447c62d8faf87427877c5b2b978eec9fc
Akihiro ITO <akihiro@harapeco.club>
2026-05-29 23:29:43 +0900

    refs #45 千野ツアー東京公演神戸公演の定義ファイルを作成する

--- app/definitions/definitions.ts
+++ app/definitions/definitions.ts
@@ -1,7 +1,9 @@
 import type { Tour } from '@/app/types';
 import FUJITA from './20260712_fujita';
 import OPERA_CITY from './20260910_opera_city';
+import KOBE_BOUKEN from './20260705_chino_kobe';
+import TOKYO_BOUKEN from './20260724_chino_tokyo';
 
-const TOURS: Tour[] = [FUJITA, OPERA_CITY];
+const TOURS: Tour[] = [FUJITA, OPERA_CITY, KOBE_BOUKEN, TOKYO_BOUKEN];
 
 export default TOURS;
