4b6bc25582aadbabc05654b1af2a188d25971873
Akihiro ITO <akihiro@harapeco.club>
2026-06-30 21:20:44 +0900

    refs #105 案件定義を追加する
    20260727 夏休み親子で楽しむクラシック音楽会
    20260826 プレミアムクラシックボリューム2
    20260826 米津真浩
    20261108 タクティカートオーケストラ

---
+++ app/definitions/20260727_oyako_classic.ts
@@ -0,0 +1,47 @@
+import type { Tour, Concert, Campaign } from '../types';
+
+const CONCERTS: Concert[] = [
+  {
+    name: '夏休み 親子で楽しむクラシック音楽会',
+    short_name: '夏休み 親子で楽しむクラシック音楽会',
+    date_at: new Date('2026-07-27T15:00:00+09:00'),
+    hall: '浜離宮朝日ホール',
+    tickets: [
+      {
+        name: '大人',
+      },
+    ],
+    distribution: [
+      {
+        campaign: '一般販売',
+        play_guide: 'teket',
+        ticket: '大人',
+        campaign_alias: '一般販売',
+        ticket_alias: '大人',
+      },
+    ],
+  },
+];
+
+const CAMPAIGNS: Campaign[] = [
+  {
+    campaign_name: '一般販売',
+    campaign_type: 'FirstCome', // 先着販売
+    reservation_start_at: null,
+    reservation_end_at: null,
+    lottery_at: null,
+    settlement_at: null,
+    sales_start_at: null,
+  },
+];
+
+const TOUR: Tour = {
+  event_code: '20260727_oyako_classic',
+  name: '夏休み 親子で楽しむクラシック音楽会',
+  short_name: '夏休み 親子で楽しむクラシック音楽会',
+  campaigns: CAMPAIGNS,
+  concerts: CONCERTS,
+  line_link_key: 'メロンココナッツ',
+};
+
+export default TOUR;
--- app/definitions/definitions.ts
+++ app/definitions/definitions.ts
@@ -9,6 +9,11 @@ import FAFNER from './20260820_fafner';
 import OHARA_TAMAI from './20260824_ohara_tamai';
 import SHIMATANI from './20260718_shimatani';
 import SUGAI from './20260719_sugai';
+import OYAKO_CLASSIC from './20260727_oyako_classic';
+import YONEZU from './20260826_yonezu';
+import TACTICART_ORCHESTRA from './20261108_tacticart_orchestra';
+import PREMIUM2 from './20260826_premium2';
+
 import TEST from './test_data';
 
 const TOURS: Tour[] = [
@@ -22,6 +27,10 @@ const TOURS: Tour[] = [
   OHARA_TAMAI,
   SHIMATANI,
   SUGAI,
+  OYAKO_CLASSIC,
+  YONEZU,
+  PREMIUM2,
+  TACTICART_ORCHESTRA,
   TEST,
 ];
 
