bc41833c5728b362bddae6d20af5918f768915b9
Akihiro ITO <akihiro@harapeco.club>
2026-07-01 17:31:47 +0900

    refs #105 LINEの文字数制限を順守する
    イベント名20文字まで

--- app/definitions/20260826_yonezu.ts
+++ app/definitions/20260826_yonezu.ts
@@ -57,7 +57,7 @@ const CAMPAIGNS: Campaign[] = [
 const TOUR: Tour = {
   event_code: '20260826_yonezu',
   name: '米津真浩 ピアノリサイタルシリーズVol.1',
-  short_name: '米津真浩 ピアノリサイタルシリーズVol.1',
+  short_name: '米津真浩 リサイタルVol.1',
   campaigns: CAMPAIGNS,
   concerts: CONCERTS,
   line_link_key: 'キウイミント',
