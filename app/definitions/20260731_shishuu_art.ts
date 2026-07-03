267295f5951f8254bd73ee1f6ede3a40813db448
Akihiro ITO <akihiro@harapeco.club>
2026-07-04 01:33:47 +0900

    refs #105 刺繍アートのconcert_short_nameを表示にも都合のよう形に修正する

--- app/definitions/20260731_shishuu_art.ts
+++ app/definitions/20260731_shishuu_art.ts
@@ -2,8 +2,8 @@ import type { Tour, Concert, Campaign } from '../types';
 
 const CONCERTS: Concert[] = [
   {
-    name: '展示会',
-    short_name: '糸と音を紡ぐ 刺繍アート展【展示会】',
+    name: '糸と音を紡ぐ 刺繍アート展【展示会】',
+    short_name: '展示会',
     date_at: new Date('2026-07-31T10:00:00+09:00'),
     hall: '浜離宮朝日ホール 小ホール',
     tickets: [
@@ -42,8 +42,8 @@ const CONCERTS: Concert[] = [
     ],
   },
   {
-    name: '演奏会',
-    short_name: '糸と音を紡ぐ 刺繍アート展【演奏会】',
+    name: '糸と音を紡ぐ 刺繍アート展【演奏会】',
+    short_name: '演奏会',
     date_at: new Date('2026-07-31T19:00:00+09:00'),
     hall: '浜離宮朝日ホール 小ホール',
     tickets: [
