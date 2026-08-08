296bcc7f4d30f2a5926c90338fe7ab942f194559
akihiro ITO <akihiro@harapeco.club>
2026-08-09 03:51:11 +0900

    refs #132 使用していない環境変数を削除するなど

--- app/process.d.ts
+++ app/process.d.ts
@@ -2,10 +2,10 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly VERCEL_ENV: 'production' | 'preview' | 'development' | undefined;
     readonly CIEL_ID: string;
+    readonly CIEL_API_SECRET: string;
     readonly CRON_SECRET: string;
     readonly CHANNEL_ACCESS_TOKEN: string;
     readonly CHANNEL_SECRET: string;
-    readonly SPREADSHEETS_API_KEY: string;
     readonly ASANA_ACCESS_TOKEN: string;
   }
 }
