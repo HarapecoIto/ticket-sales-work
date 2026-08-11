1e25b20e0e38513db7c20116d3b25d621d18e0ae
akihiro ITO <akihiro@harapeco.club>
2026-08-11 19:18:59 +0900

    refs #133 古い案件の定義を削除する

--- app/definitions/definitions.ts
+++ app/definitions/definitions.ts
@@ -1,37 +1,23 @@
 import type { Tour } from '@/app/types';
-import FUJITA from './20260712_fujita';
 import OPERA_CITY from './20260910_opera_city';
-import TOKYO_BOUKEN from './20260724_chino_tokyo';
-import SHIN_SUMMER from './20260719_shin_summer';
 import TABICLA from './20260825_tabicla';
 import FAFNER from './20260820_fafner';
 import OHARA_TAMAI from './20260824_ohara_tamai';
-import SHIMATANI from './20260718_shimatani';
-import SUGAI from './20260719_sugai';
-import OYAKO_CLASSIC from './20260727_oyako_classic';
 import YONEZU from './20260826_yonezu';
 import TACTICART_ORCHESTRA from './20261108_tacticart_orchestra';
 import PREMIUM2 from './20260826_premium2';
-import SHISHUU_ART from './20260731_shishuu_art';
 import HAKKUTSU_VOL2 from './20260903_hakkutsu_vol2';
 import TEST from './test_data';
 
 const TOURS: Tour[] = [
-  FUJITA,
   OPERA_CITY,
-  TOKYO_BOUKEN,
-  SHIN_SUMMER,
   TABICLA,
   FAFNER,
   OHARA_TAMAI,
-  SHIMATANI,
-  SUGAI,
-  OYAKO_CLASSIC,
   YONEZU,
   PREMIUM2,
   TACTICART_ORCHESTRA,
   HAKKUTSU_VOL2,
-  SHISHUU_ART,
   TEST,
 ];
 
